88af77d04c430d938efba04b06e2409305837fd8
Michael Francis Brown <m1ch4@m1ch4-comp.cable.rcn.com>
2018-07-11 13:43:22 -0400

    created game logic

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/* memory game JS code */
+
+/*assign card values */
+var cardOne = "queen";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "king";
+/*prints value */ 
+console.log("User flipped " + cardOne);
