998406b1e7e08ad0c1df2d4c88b01aca57a5ebb1
Michael Francis Brown <m1ch4@m1ch4-comp.cable.rcn.com>
2018-07-12 10:26:33 -0400

    added game logic

--- js/main.js
+++ js/main.js
@@ -1,24 +1,44 @@
 /* memory game JS code */
-
-var cards = ["queen", "queen", "king", "king"];
+/* Cards in Play */
 var cardsInPlay = [];
+/* "Deck" of Cards */
+var cards = [
+  {
+  rank: "queen",
+  suit: "hearts",
+  cardImage: "images/queen-of-hearts.png"
+},
+  {rank: "queen",
+  suit: "diamonds",
+  cardImage: "images/queen-of-diamonds.png"
+},
+  {rank: "king",
+  suit: "hearts",
+  cardImage: "images/king-of-hearts.png"
+},
+  {rank: "king",
+  suit: "diamonds",
+  cardImage: "images/king-of-diamonds.png"}
+];
 
-/* */
+/* Flip a Card*/
+var flipCard = function(cardId) {
+cardsInPlay.push(cards[cardId].rank);
+console.log("User flipped " + cards[cardId].rank);
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);
+}
+flipCard(0);
+flipCard(2);
+
+/* Check for Match */
 var checkForMatch = function() {
-  if(cardsInPlay[0] === cardsInPlay[1]) {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
     console.log("You found a match!");
   }
   else {
     console.log("Sorry, please try again.");
-  };
+  }
 };
 
-/* */
-var flipCard = function(cardId) {
-cardsInPlay.push(cards[cardId]);
-console.log("User flipped " + cards[cardId]);
-;
-}
-flipCard(0);
-flipCard(2);
-checkForMatch(); 
+checkForMatch()
