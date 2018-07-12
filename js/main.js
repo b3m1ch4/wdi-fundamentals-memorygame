d1af4537ec9ec4c01b81501ab2a7d7436c5774b9
Michael Francis Brown <m1ch4@m1ch4-comp.cable.rcn.com>
2018-07-11 22:36:25 -0400

    created more game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,24 @@
 /* memory game JS code */
 
-/*assign card values */
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-/*prints value */ 
-console.log("User flipped " + cardOne);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+/* */
+var checkForMatch = function() {
+  if(cardsInPlay[0] === cardsInPlay[1]) {
+    console.log("You found a match!");
+  }
+  else {
+    console.log("Sorry, please try again.");
+  };
+};
+
+/* */
+var flipCard = function(cardId) {
+cardsInPlay.push(cards[cardId]);
+console.log("User flipped " + cards[cardId]);
+;
+}
+flipCard(0);
+flipCard(2);
+checkForMatch(); 
